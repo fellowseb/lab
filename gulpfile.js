e3647159dc4f38a887e067ee5383ff438b998aa0
seb <seb@seb.com>
2018-02-05 21:00:10 +0100

    Make html gulp task copy fonts and images along with html

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var configuration = {
             fonts: [ './src/fonts/*.ttf',
                 './src/fonts/*.woff',
                 './src/fonts/*.woff2' ],
+            images: './src/images/*.*',
             scss: './src/sass/*.scss'
         },
         dist: './dist'
@@ -20,19 +21,14 @@ var configuration = {
 function htmlTask(done) {
     gulp.src(configuration.paths.src.html)
         .pipe(gulp.dest(configuration.paths.dist));
+    gulp.src(configuration.paths.src.fonts)
+        .pipe(gulp.dest(configuration.paths.dist + '/fonts'));
+    gulp.src(configuration.paths.src.images)
+        .pipe(gulp.dest(configuration.paths.dist + '/images'));
     done();
 }
 htmlTask.description = 'Copy HTML files to output';
 gulp.task('html', htmlTask);
-
-// Gulp task to copy font files to output directory
-function fontTask(done) {
-    gulp.src(configuration.paths.src.fonts)
-        .pipe(gulp.dest(configuration.paths.dist + '/fonts'));
-    done();
-}
-fontTask.description = 'Copy font files to output';
-gulp.task('fonts', fontTask);
 
 // Gulp task to generate CSS from SCSS files
 function sassTask() {
@@ -55,4 +51,4 @@ cleanTask.description = 'Clean dist folder';
 gulp.task('clean', cleanTask);
 
 // Gulp build task
-gulp.task('build', gulp.series('html', 'fonts', 'sass'));
+gulp.task('build', gulp.series('html', 'sass'));
