4cb8edc28c64e381d3d52450ff53423beb152885
fellowseb <wauquier.sebastien@gmail.com>
2018-04-21 21:16:13 +0200

    Add current nav link update on scroll event

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -58,6 +58,16 @@ function onMenuBtnClick(event) {
     event.stopPropagation();
 }
 
+function setCurrentNav(menuLinkID) {
+    var $menunav = document.getElementById('menu-nav');
+    var $menunavitem = $menunav.querySelector('a[id=' + menuLinkID + ']');
+    var $menunavlinks = $menunav.querySelectorAll('.menu-nav-link');
+    Array.prototype.forEach.call($menunavlinks, function removeActiveClass($menunavlink, i) {
+        $menunavlink.classList.remove('menu-nav-link-current');
+    });
+    $menunavitem.classList.add('menu-nav-link-current');
+}
+
 /**
  * Handler called when a click on a nav item happens.
  *
@@ -71,6 +81,22 @@ function onNavItemClick(event) {
             event.preventDefault();
             var anchorID = event.target.href.substr(
                 event.target.href.indexOf('#') + 1);
+            var urlShardPos = window.location.href.lastIndexOf('#');
+            if (urlShardPos) {
+                window.history.pushState({
+                    currentMenuLink: event.target.id
+                },
+                document.title,
+                window.location.href.substr(0, urlShardPos) + '#' + anchorID);
+            } else {
+                window.history.pushState({
+                    currentMenuLink: event.target.id
+                },
+                document.title,
+                window.location.href + '#' + anchorID);
+            }
+            setCurrentNav(event.target.id);
+            event.target.classList.add('menu-nav-link-current');
             var anchorElem = document.querySelectorAll('a[name=' + anchorID + ']')[0];
             window.scrollBy({
                 top: anchorElem.offsetTop - window.pageYOffset,
@@ -80,6 +106,56 @@ function onNavItemClick(event) {
         }
     }
     event.stopPropagation();
+}
+
+/**
+ * Handler called when history changes
+ */
+function onPopState() {
+    var currentState = window.history.state;
+    if (currentState.currentMenuLink) {
+        setCurrentNav(currentState.currentMenuLink);
+    }
+}
+
+function findMenuNavLink(anchorName) {
+    var menuNavLinks = document.querySelectorAll('a.menu-nav-link');
+    return Array.prototype.find.call(menuNavLinks, function findNavElem($menuNavLink) {
+        return $menuNavLink.href.substr($menuNavLink.href.lastIndexOf('#') + 1) === anchorName;
+    });
+}
+
+function updateCurrentNav() {
+    var $anchors = document.querySelectorAll('a.anchor');
+    var mostVisiblePage = Array.prototype.map.call(
+        $anchors,
+        function mapToPageVisibleHeight($anchor, i, arr) {
+            var pageBottom = i + 1 < arr.length
+                ? arr[i + 1].offsetTop
+                : document.documentElement.scrollHeight;
+            var visibleBottom = Math.min(pageBottom, window.pageYOffset + window.innerHeight);
+            var pageTop = $anchor.offsetTop;
+            var visibleTop = Math.max(pageTop, window.pageYOffset);
+            return {
+                visiblePageHeight: visibleBottom - visibleTop,
+                anchor: $anchor
+            };
+        }).sort(function sortPageVisibleHeight(lhs, rhs) {
+        return lhs.visiblePageHeight < rhs.visiblePageHeight;
+    })[0];
+    if (mostVisiblePage && mostVisiblePage.anchor) {
+        var menuNavElem = findMenuNavLink(mostVisiblePage.anchor.name);
+        if (menuNavElem) {
+            setCurrentNav(menuNavElem.id);
+        }
+    }
+}
+
+/**
+ * Handler called when user scrolls
+ */
+function onScroll() {
+    updateCurrentNav();
 }
 
 /**
@@ -125,8 +201,13 @@ function onNavBgdTouchMove(event) {
         $menunav.addEventListener('click', onNavBgdClick);
         $menunav.addEventListener('touchmove', onNavBgdTouchMove, false);
         var $menunavlinks = $menunav.querySelectorAll('.menu-nav-link');
-        Array.prototype.forEach.call($menunavlinks, function ($menunavlink, i) {
+        Array.prototype.forEach.call($menunavlinks, function addListenerMenuLink($menunavlink, i) {
             $menunavlink.addEventListener('click', onNavItemClick);
         });
+    }
+    window.addEventListener('popstate', onPopState);
+    window.addEventListener('scroll', onScroll);
+    if (window.pageYOffset === 0) {
+        updateCurrentNav();
     }
 });
