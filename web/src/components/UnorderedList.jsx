815dfc0bfd487a8a3846561dd517bca324c1501a
fellowseb <wauquier.sebastien@gmail.com>
2019-03-01 21:18:52 +0100

    fix(web): remove unused UnorderedList component

--- web/src/components/UnorderedList.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const UnorderedList = styled.ul `
-  list-style: square;
-  padding: 0 1em;
-  list-style-position: inside;
-`
-
-export default UnorderedList;
