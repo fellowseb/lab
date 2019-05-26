7ff21f3363087e29803807a65ea857544d250e65
fellowseb <wauquier.sebastien@gmail.com>
2019-05-26 14:51:30 +0200

    style(web): reformat deploy script

--- web/deploy.sh
+++ web/deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
   echo "Which folder do you want to deploy to GitHub Pages?"
   exit 1
 fi
-if [ -f "$1/.dev" ]
-then
+if [ -f "$1/.dev" ]; then
   echo "Can't deploy a non prod build."
   exit 1
 fi
